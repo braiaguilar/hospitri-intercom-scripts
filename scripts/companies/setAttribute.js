be6c79f869da59c8a67db90523542721dcd17fa2
braiaguilar <brianisaiasaguilar@gmail.com>
2025-06-16 14:34:59 -0300

    [Feature] - setAttribute adapted to multiple listings per company update

--- scripts/companies/setAttribute.js
+++ scripts/companies/setAttribute.js
@@ -2,28 +2,61 @@ const intercomClient = require('../../utils/intercomClient');
 
 /**
  * Asigns value of a custom attribute to company IDs array
- * @param {Array<string>} companyIds - Intercom Companies IDs array
+ * @param {Array<string>} companyIds - Array of Intercom Company IDs
  * @param {string} attribute - Custom attribute to set (e.g., 'active')
  * @param {any} value - Value to set for the custom attribute (e.g., true/false)
  * @returns {Promise<Array>} - Array of results indicating success or failure for each company ID
  */
-async function setAttribute(companyIds, attribute, value) {
+const setAttribute = async (companyIds, attribute, value) => {
     const results = [];
-
-    for (const id of companyIds) {
+    if (attribute === 'listings') {
         try {
-            const res = await intercomClient.put(`/companies/${id}`, {
-                custom_attributes: {
-                    [attribute]: value,
-                },
-            });
-            results.push({ id, success: true });
+            if (companyIds.length !== value.length) {
+                throw new Error(
+                    'Company IDs and values arrays must have the same length'
+                );
+            }
+            for (let i = 0; i < companyIds.length; i++) {
+                try {
+                    await intercomClient.put(
+                        `/companies/${companyIds[i]}`,
+                        {
+                            custom_attributes: {
+                                [attribute]: value[i],
+                            },
+                        }
+                    );
+                    results.push({ id: companyIds[i], success: true });
+                } catch (error) {
+                    console.error(
+                        `Error setting attribute value for company ${companyIds[i]}:`,
+                        error.response?.data || error.message
+                    );
+                    results.push({ id: companyIds[i], success: false });
+                }
+            }
         } catch (error) {
             console.error(
-                `Error setting attribute value for company ${id}:`,
+                `Error setting listings:`,
                 error.response?.data || error.message
             );
-            results.push({ id, success: false });
+        }
+    } else {
+        for (const id of companyIds) {
+            try {
+                await intercomClient.put(`/companies/${id}`, {
+                    custom_attributes: {
+                        [attribute]: value,
+                    },
+                });
+                results.push({ id, success: true });
+            } catch (error) {
+                console.error(
+                    `Error setting attribute value for company ${id}:`,
+                    error.response?.data || error.message
+                );
+                results.push({ id, success: false });
+            }
         }
     }
 
@@ -38,4 +71,6 @@ async function setAttribute(companyIds, attribute, value) {
 
 // Use script
 const companiesToUpdate = [];
-setAttribute(companiesToUpdate, 'active', false);
+const value = [];
+
+setAttribute(companiesToUpdate, 'listings', value);
